05c91c397b60ab98dc793d55ec3bfa9545f972c2
bacoords <bacoords@gmail.com>
2016-03-08 11:11:40 -0800

    add rest support

--- cas-wp-plugin.js
+++ cas-wp-plugin.js
@@ -19,7 +19,7 @@
 
 angular.module('backendApp', ['wp.api'])
 
-.controller('backendCtrl', function($scope, $http, wpAPIResource, wpAPIData){
+.controller('backendCtrl', function($scope, $http, wpAPIResource){
 //  $http.get('http://cas.threecordsstudio.com/wp-json/wp/v2/cas_school')
 //    .then(function(response){
 //    $scope.schools = response.data;
