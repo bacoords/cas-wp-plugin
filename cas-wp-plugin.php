d1adb5c438c0fd8237f844b588d2c93ad63aa801
bacoords <bacoords@gmail.com>
2016-03-07 22:29:36 -0800

    add rest support

--- cas-wp-plugin.php
+++ cas-wp-plugin.php
@@ -138,4 +138,25 @@ class PageTemplater {
 
 add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
 
+
+
+  /**
+  * Add REST API support to an already registered post type.
+  */
+  add_action( 'init', 'cas_school_post_type_rest_support', 25 );
+  function cas_school_post_type_rest_support() {
+    global $wp_post_types;
+
+    //be sure to set this to the name of your post type!
+    $post_type_name = 'cas_school';
+    if( isset( $wp_post_types[ $post_type_name ] ) ) {
+        $wp_post_types[$post_type_name]->show_in_rest = true;
+        $wp_post_types[$post_type_name]->rest_base = $post_type_name;
+        $wp_post_types[$post_type_name]->rest_controller_class = 'WP_REST_Posts_Controller';
+    }
+
+  }
+
+
+
 ?>
