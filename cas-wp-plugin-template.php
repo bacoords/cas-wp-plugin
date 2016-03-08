bcf7d1eac204b0e1adc328a5b247ebac108f7be9
bacoords <bacoords@gmail.com>
2016-03-08 14:59:28 -0800

    update css

--- cas-wp-plugin-template.php
+++ cas-wp-plugin-template.php
@@ -50,6 +50,8 @@ get_header();
     <div class="cwp-modal-overlay" ng-show="isShowingModal">
       <div class="cwp-modal">
         <div class="cwp-modal__header">
+           {{modalSchool._cas_school_name}} <br>
+           {{school._cas_school_city}},  {{school._cas_school_state}}
           <a class="cwp-button" ng-click="showModal(-1)">Close</a>
         </div>
          <div class="cwp-modal__body">
