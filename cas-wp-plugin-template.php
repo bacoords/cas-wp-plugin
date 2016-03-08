46ec53ba73356fd9d22029c2a2f89283d9e5d737
bacoords <bacoords@gmail.com>
2016-03-08 13:56:33 -0800

    update css

--- cas-wp-plugin-template.php
+++ cas-wp-plugin-template.php
@@ -6,15 +6,19 @@ get_header();
   <div ng-controller="backendCtrl">
    
    <div class="cwp-header">
+<!--
      <div class="cwp-header__logo">
        CAS
      </div>
+-->
      <div class="cwp-header__search">
        <input type="text" ng-model="search" class="cwp-search" placeholder="Search Our Schools">
      </div>
+<!--
      <div class="cwp-header__calendar">
        CALENDAR THING
      </div>
+-->
    </div>
    <BR><BR><BR><BR>
    
@@ -27,15 +31,27 @@ get_header();
         <td>Links</td>
       </tr>
       <tr ng-repeat="school in schools | filter:search | orderBy:'_cas_school_name'">
-        <td><a class="cwp-button cwp-button--blue">+info</a><a class="cwp-button">email</a></td>
+        <td>
+          <a class="cwp-button cwp-button--blue">+info</a>
+          <a class="cwp-button">email</a>
+        </td>
         <td> {{school._cas_school_name}}</td>
         <td> {{school._cas_school_city}},  {{school._cas_school_state}}</td>
         <td>{{school.modified | date: 'M/d/yy'}}</td> 
-        <td><a ng-href="{{school.link}}" class="cwp-button" target="_blank">CAS</a><a ng-href="school._cas_school_team_bank_url" ng-if="school._cas_school_team_bank_url" class="cwp-button" target="_blank">TB</a></td>
+        <td>
+          <a ng-href="{{school.link}}" class="cwp-button" target="_blank">CAS</a>
+          <a ng-href="{{school._cas_school_team_bank_url}}" class="cwp-button" target="_blank">TB</a>
+        </td>
       
       </tr>
     </table>
     <BR><BR><BR><BR><BR>
+    
+    <div class="cwp-modal-overlay">
+      <div class="cwp-modal">
+        TEST
+      </div>
+    </div>
     
   </div>
 
--- cas-wp-plugin.php
+++ cas-wp-plugin.php
@@ -158,7 +158,9 @@ function cas_plugin_enqueue_script() {
 add_action( 'wp_enqueue_scripts', 'cas_plugin_enqueue_script' );
 
 function cas_plugin_enqueue_style() {
+  if(is_page_template( 'cas-wp-plugin-template.php')){
     wp_enqueue_style( 'cas-plugin-style', 'http://cas.threecordsstudio.com/wp-content/plugins/cas-wp-plugin/dist/css/cas-wp-plugin.css' ); 
+  }
 }
 add_action( 'wp_enqueue_scripts', 'cas_plugin_enqueue_style' );
 
