43c213d7d2ca61714f2b619dbee29c8d0d0e7229
bacoords <bacoords@gmail.com>
2016-03-10 12:07:08 -0800

    update cal page

--- cas-wp-plugin.php
+++ cas-wp-plugin.php
@@ -427,7 +427,22 @@ function cwp_create_post_type() {
 	);
 }
 
+  /**
+  * Add REST API support to an already registered post type.
+  */
+  add_action( 'init', 'cas_email_template_post_type_rest_support', 25 );
+  function cas_email_template_post_type_rest_support() {
+    global $wp_post_types;
 
+    //be sure to set this to the name of your post type!
+    $post_type_name = 'cas_email_template';
+    if( isset( $wp_post_types[ $post_type_name ] ) ) {
+        $wp_post_types[$post_type_name]->show_in_rest = true;
+        $wp_post_types[$post_type_name]->rest_base = $post_type_name;
+        $wp_post_types[$post_type_name]->rest_controller_class = 'WP_REST_Posts_Controller';
+    }
+
+  }
 
 
 /**
