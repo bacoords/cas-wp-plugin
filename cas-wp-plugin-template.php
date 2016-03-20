8bc64fd1c2bb334a13a76f4163c7d3c99b525b83
bacoords <bacoords@gmail.com>
2016-03-20 10:36:02 -0700

    email updates

--- cas-wp-plugin-template.php
+++ cas-wp-plugin-template.php
@@ -22,7 +22,10 @@ if (!empty($_POST)){
     $cc = 'Cc: ' . $_POST['cc'];
     $header[] = $cc;
   }
-  $sent = wp_mail( $to, $_POST['subject'], $_POST['emailBody'], $headers );
+  
+  $body = str_replace("=", "=3D", $_POST['emailBody'])
+  
+  $sent = wp_mail( $to, $_POST['subject'], $body, $headers );
   
 }
 
@@ -340,7 +343,7 @@ if(is_user_logged_in ()){ ?>
               <input type="text" name="from" placeholder="From Email Address">
               <input type="text" name="cc" placeholder="CC">
               <input type="text" name="subject" ng-model="emailSubject" placeholder="Subject">
-              <?php wp_editor('Select a Template', 'tab-editor', array('editor_height'=>'500px','textarea_name'=>'emailBody')); ?>
+              <?php wp_editor('Select a Template', 'tab-editor', array('editor_height'=>'500px','wpautop'=>false,'textarea_name'=>'emailBody')); ?>
 <!--              <textarea ng-model="emailBody"></textarea>-->
 <!--              <a ng-click="emailSubmit()" class="cwp-button cwp-button--email-submit" target="_blank">SEND EMAIL</a>-->
               <input type="submit" class="cwp-button cwp-button--email-submit" target="_blank">SEND EMAIL</a>
