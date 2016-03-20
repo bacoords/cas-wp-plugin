3dcdedbaa2490b09598def4961ba102b5dc4cf73
bacoords <bacoords@gmail.com>
2016-03-20 10:05:51 -0700

    email updates

--- cas-wp-plugin-template.php
+++ cas-wp-plugin-template.php
@@ -18,7 +18,7 @@ if (!empty($_POST)){
   'From: Community All Stars <' . $_POST['from'] . '>;',
   );
   
-  if(!empty($_POST['cc'])){
+  if(isset($_POST['cc'])){
     $cc = 'CC: ' . $_POST['cc'] . ';'
     array_push($headers, $cc);
   }
