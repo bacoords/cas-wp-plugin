03af86bc71f91167e4256299f08491656c5132ab
bacoords <bacoords@gmail.com>
2016-03-23 15:14:15 -0700

    final updates

--- cas-wp-plugin.js
+++ cas-wp-plugin.js
@@ -124,7 +124,7 @@ angular.module('backendApp', ['wp.api'])
     $scope.emailSelect = e;
     jQuery('.cwp-button--email-template').removeClass('cwp-button--email-template__selected');
     jQuery('#email-link-' + e.id).addClass('cwp-button--email-template__selected');    
-    var r = e._cas_email_template_subject.replace(/<br\s*[\/]?>/gi, "%0A");
+    var r = e._cas_email_template_subject;
     var r = r.replace(/\[SCHOOL\]/g, $scope.emailSchool._cas_school_name);
     var r = r.replace(/\[TITLE\]/g, $scope.emailSchool._cas_school_contact_title);
     var r = r.replace(/\[CONTACT\]/g, $scope.emailSchool._cas_school_contact_name);
@@ -134,9 +134,9 @@ angular.module('backendApp', ['wp.api'])
     var r = r.replace(/\[SPONSOR\]/g, $scope.emailToName);      
 //    var r = encodeURIComponent(r);
     $scope.emailSubject = r;
-    var q = e.content.rendered.replace(/<br\s*[\/]?>/gi, "%0A");
-    var q = e.content.rendered.replace(/<p\s*[\/]?>/gi, "%0A");
-    var q = e.content.rendered.replace(/<\/p\s*[\/]?>/gi, "%0A");
+    var q = e.content.rendered.replace(/<br\s*[\/]?>/gi, "%0D%0A");
+    var q = q.replace(/<p\s*[\/]?>/gi, "%0D%0A");
+    var q = q.replace(/<\/p\s*[\/]?>/gi, "%0D%0A");
     var q = q.replace(/\[SCHOOL\]/g, $scope.emailSchool._cas_school_name);
     var q = q.replace(/\[TITLE\]/g, $scope.emailSchool._cas_school_contact_title);
     var q = q.replace(/\[CONTACT\]/g, $scope.emailSchool._cas_school_contact_name);
