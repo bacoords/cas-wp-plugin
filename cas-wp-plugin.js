9b37ab2e08795623ba0ef3de3f3acb67e9b043d4
bacoords <bacoords@gmail.com>
2016-03-24 08:01:01 -0700

    more

--- cas-wp-plugin.js
+++ cas-wp-plugin.js
@@ -225,15 +225,19 @@ angular.module('backendApp', ['wp.api'])
     $scope.nearbySchoolsEmailStr = '';
     for(var i = 0; i < b.length; i++){
       var c = b.length - 1;
+      console.log(c);
       wpAPIResource.get( {
         param1: 'cas_school',
         param2: b[i]
       } ).$promise.then(function(result){
         $scope.nearbySchoolsEmailStr += result.title.rendered;
-        if (i < c){
+
+        console.log('c = ' + c);
+        console.log('i = ' + i);
+        if(i < c){
           $scope.nearbySchoolsEmailStr += ', ';
         }
-        if (i === c){
+        if(i === c){
           $scope.nearbySchoolsEmailStr += ' and ';
         }
       });
